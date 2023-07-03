d1eb129b1981723d225d641774a1da35730425b4
matheus-fraga <matheuscarlosfraga@gmail.com>
2023-07-03 12:02:55 -0300

    variaveis

---
+++ aula-030723/variaveis.js
@@ -0,0 +1,31 @@
+// Java -> Linguagem de programa (Compilada)
+// JavaScript -> Linguagem de programação (Interpretada)
+
+/* comentário
+ de multiplas 
+ linhas*/
+// comentário de única linha
+
+// Chrome -> Interpretador do JS
+// Node.js -> V8 + NPM (Node Package Manager)
+
+// console.log("Texto"); // imprime no terminal/console uma informação.
+// console.log(4)
+
+// Variáveis -> Espaço (na memória) para armazenar um valor.
+let nome;
+nome = "Matheus Fraga";
+console.log(nome);
+
+let idade = 23; // declaração da variável e atribuição do valor.
+console.log(idade);
+
+let altura = 1.78;
+console.log(idade);
+
+let cpf = '123.456.789-00';
+console.log(cpf);
+
+let eCasado = true; // false e true
+console.log(eCasado);
+
